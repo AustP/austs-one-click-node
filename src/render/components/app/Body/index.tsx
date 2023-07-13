7d3ae9b2f0d384c4481e75f199cd5231d0b50680
Aust <austp17@gmail.com>
2023-07-13 17:11:27 -0600

    Make step names better

--- src/render/components/app/Body/index.tsx
+++ src/render/components/app/Body/index.tsx
@@ -20,59 +20,63 @@ export default function Body() {
       <Column className="w-1/3">
         <div className="font-light mb-6 text-xl">Node Overview</div>
         <StatusIndicator
-          active={step === Step.Docker_Installed}
+          active={step === Step.Check_Docker_Installed}
           label="Docker Installed"
-          status={stepStatus[Step.Docker_Installed]}
+          status={stepStatus[Step.Check_Docker_Installed]}
         />
         <StatusIndicator
           active={
-            step === Step.Container_Built || step === Step.Container_Building
+            step === Step.Check_Container_Built ||
+            step === Step.Container_Building
           }
           className="mt-2"
           label="Container Built"
           status={
-            step === Step.Container_Built
-              ? stepStatus[Step.Container_Built]
+            step === Step.Check_Container_Built
+              ? stepStatus[Step.Check_Container_Built]
               : stepStatus[Step.Container_Building]
           }
         />
         <StatusIndicator
           active={
-            step === Step.Container_Running || step === Step.Container_Starting
+            step === Step.Check_Container_Running ||
+            step === Step.Container_Starting
           }
           className="mt-2"
           label="Container Running"
           status={
-            step === Step.Container_Running
-              ? stepStatus[Step.Container_Running]
+            step === Step.Check_Container_Running
+              ? stepStatus[Step.Check_Container_Running]
               : stepStatus[Step.Container_Starting]
           }
         />
         <StatusIndicator
-          active={step === Step.Node_Running || step === Step.Node_Starting}
+          active={
+            step === Step.Check_Node_Running || step === Step.Node_Starting
+          }
           className="mt-2"
           label="Node Running"
           status={
-            step === Step.Node_Running
-              ? stepStatus[Step.Node_Running]
+            step === Step.Check_Node_Running
+              ? stepStatus[Step.Check_Node_Running]
               : stepStatus[Step.Node_Starting]
           }
         />
         <StatusIndicator
-          active={step === Step.Node_Synced || step === Step.Node_Syncing}
+          active={step === Step.Check_Node_Synced || step === Step.Node_Syncing}
           className="mt-2"
           label="Node Synced"
           status={
-            step === Step.Node_Synced
-              ? stepStatus[Step.Node_Synced]
+            step === Step.Check_Node_Synced
+              ? stepStatus[Step.Check_Node_Synced]
               : stepStatus[Step.Node_Syncing]
           }
         />
         <StatusIndicator
-          active={step === Step.Participating}
+          active={step === Step.Dashboard}
           className="mt-2"
           label="Participating in Consensus"
-          status={stepStatus[Step.Participating]}
+          status={stepStatus[Step.Dashboard]}
         />
       </Column>
       <StepViewer
