7bed73ad6ae8c59cb842c2c884fbd59ddffc6580
Aust <austp17@gmail.com>
2023-07-14 13:10:41 -0600

    Fix height adjustment with system buttons

--- src/render/components/app/Header/index.tsx
+++ src/render/components/app/Header/index.tsx
@@ -28,37 +28,49 @@ export default function Header() {
 
   return (
     <div className="bg-sky-200 dark:bg-sky-800">
-      <div className="flex items-center">
+      <div className="flex h-6 items-center">
         {isMac ? (
           <>
             <div
-              className="cursor-pointer pl-2 pr-1 py-2"
+              className="cursor-pointer flex h-full items-center justify-center pl-2 px-1"
               onClick={window.electron.quit}
             >
               <div className="bg-[#ff605c] h-3 rounded-full shrink-0 w-3" />
             </div>
             <div
-              className="cursor-pointer px-1 py-2"
+              className="cursor-pointer flex h-full items-center justify-center px-1"
               onClick={window.electron.minimize}
             >
               <div className="bg-[#ffbd44] h-3 rounded-full shrink-0 w-3" />
             </div>
-            <div className="cursor-pointer px-1 py-2" onClick={handleMaximize}>
+            <div
+              className="cursor-pointer flex h-full items-center justify-center px-1"
+              onClick={handleMaximize}
+            >
               <div className="bg-[#00ca4e] h-3 rounded-full shrink-0 w-3" />
             </div>
-            <div className="grow self-stretch [-webkit-app-region:drag]" />
+            <div className="grow h-full [-webkit-app-region:drag]" />
           </>
         ) : (
           <>
-            <div className="grow self-stretch [-webkit-app-region:drag]" />
-            <div className="cursor-pointer" onClick={window.electron.minimize}>
-              <MinusIcon className="cursor-pointer hover:bg-sky-300 dark:hover:bg-sky-700 p-1 shrink-0 hover:text-black dark:hover:text-white transition w-12" />
+            <div className="grow h-full [-webkit-app-region:drag]" />
+            <div
+              className="cursor-pointer flex h-full items-center justify-center"
+              onClick={window.electron.minimize}
+            >
+              <MinusIcon className="cursor-pointer hover:bg-sky-300 dark:hover:bg-sky-700 h-full shrink-0 hover:text-black dark:hover:text-white transition w-12" />
             </div>
-            <div className="cursor-pointer" onClick={handleMaximize}>
-              <SquareIcon className="cursor-pointer hover:bg-sky-300 dark:hover:bg-sky-700 p-2 shrink-0 hover:text-black dark:hover:text-white transition w-12" />
+            <div
+              className="cursor-pointer flex h-full items-center justify-center"
+              onClick={handleMaximize}
+            >
+              <SquareIcon className="cursor-pointer hover:bg-sky-300 dark:hover:bg-sky-700 h-full p-1 shrink-0 hover:text-black dark:hover:text-white transition w-12" />
             </div>
-            <div className="cursor-pointer" onClick={window.electron.quit}>
-              <XIcon className="cursor-pointer hover:bg-red-500 p-1 shrink-0 hover:text-white transition w-12" />
+            <div
+              className="cursor-pointer flex h-full items-center justify-center"
+              onClick={window.electron.quit}
+            >
+              <XIcon className="cursor-pointer hover:bg-red-500 h-full shrink-0 hover:text-white transition w-12" />
             </div>
           </>
         )}
